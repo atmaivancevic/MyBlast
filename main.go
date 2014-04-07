4f4aaf6eb1afc6c2c1c8408f3fa5954652605d0e
Atma <atma@Atma-PC.(none)>
2014-04-08 01:28:42 +0930

    made it submit a BLAST query - but it's still not sending me an email of the output?

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"code.google.com/p/biogo.ncbi/blast"
+
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-//Example Usage: MyBlast file.fasta txid[5] blastn 10 7 4,-5 12,8
+const (
+	tool  = "blast-testing"
+	email = "atma.ivancevic@gmail.com"
+	retry = 15
+)
+
+// Example Usage: MyBlast file.fasta txid[5] blastn 10 7 4,-5
 func main() {
 
 	queryFileName := os.Args[1]
@@ -36,15 +44,15 @@ func main() {
 		panic(err)
 	}
 
-	gapCostString := os.Args[7]
-	gapCostExistence, err := strconv.ParseInt((strings.Split(gapCostString, ","))[0], 0, 64)
-	if err != nil {
-		panic(err)
-	}
-	gapCostExtension, err := strconv.ParseInt((strings.Split(gapCostString, ","))[1], 0, 64)
-	if err != nil {
-		panic(err)
-	}
+	// gapCostString := os.Args[7]
+	// gapCostExistence, err := int(strconv.ParseInt((strings.Split(gapCostString, ","))[0], 0, 64))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// gapCostExtension, err := int(strconv.ParseInt((strings.Split(gapCostString, ","))[1], 0, 64))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	fmt.Println("Query File Name: " + queryFileName)
 	fmt.Println("Organism: " + organism)
@@ -53,7 +61,70 @@ func main() {
 	fmt.Printf("Word Size: %d\n", wordSize)
 	fmt.Printf("Match Score: %d\n", matchScore)
 	fmt.Printf("Mismatch Score: %d\n", mismatchScore)
-	fmt.Printf("Gap Cost - Existence: %d\n", gapCostExistence)
-	fmt.Printf("Gap COst - Extension: %d\n", gapCostExtension)
+	// fmt.Printf("Gap Cost - Existence: %d\n", gapCostExistence)
+	// fmt.Printf("Gap Cost - Extension: %d\n", gapCostExtension)
+
+	// Read the query sequence from file called queryFileName
+	query := "gcgcaggcggctgcgacgggcgcgctaagcgcggccgagaggagccaccccacgtcgaggtcaggggcagaagccgggaggaccccatgcccgaagggcggcggccaagaggagttaccccacgtccgaggtcaggggcagcggccgagagtaccagactgcgacggcgcaggaacggccgagaggagctaccccgcgtccgaggtcagggggggcggccgagaggagatacccagcgtccgaggtcaggggcggcgacgagaggagttaccccgcgtccgaggtcaggggcggcggccgggaggagctaccccacgcccctaagcccgaggccaggggcggcggccgggaggagcaaccccacgcccgaggccaggggcggcggccgggaggaccaaccccacgtccaaggagccgtggctgcgcgggcacaggagggcctagaggagctatcccacgttgaaggtcaggaagggcggcggtgaggagatacccctcgtccaaggtaaggagcagcggctgcgctttgctggagcagccgtgaagagataccccacgcccaaggtaagagaaacccaagtaagacggtaggtgttgcaagagggcatcagagggcagacacactgaaaccatactcacagaaaactagtcaatctaatcacactaggaccacagccttgtctaactcaatgaaactaagccatgcccgtggggcaacccaagatgggcgggtcatggtggagagatctgacagaatgtggtccactggagaagggaatggcaaaccacttcagtattcttgccttgagaaccccatgaacagtatgaaaaggcaaaatgataggatactgaaagaggaactccccaggtcagtaggtgcccaatatgctactggagatcagtggagaaataactccagaaagaatgaagggatggagccaaagcaaaaacaatacccagctgtggatgtgactggtgatagaagcaaggtccgatgctgtaaagagcaatattgcataggaacctggaatgtcaggtccatgaatcaaggcaaattggaagtggtcaaacaagagatggcaagagtgaatgtcgacattctaggaatcagcgaactgaaatggactggaatgggtgaatttaactcagatgaccattatatctactactgcgggcaggaatccctcagaagaaatggagtggccatcatggtcaacaaaagagtccgaaatgcagtacttggatgcaatctcaaaaacgacagaatgatctctgttcgtttccaaggcaaaccattcaatatcacagtaatccaagtctatgccccaaccagtaacgctgaagaagctgaagttgaacggttctatgaagacctacaagaccttttagaactaacacccaaaaaagatgtccttttcattataggggactggaatgcaaaagtaggaagtcaagaaacacctggagtaacaggcaaatttggccttggaatacggaatgaagcagggcaaagactaatagagttttgccaagaaaatgcactggtcatagcaaacaccctcttccaacaacacaagagaagactctatacatggacatcaccagatgtcaacaccgaaatcagattgattatattctttgcagccaaagatggagaagctctatacagtcagcaaaaacaagaccaggagctgactgtggctcagaccatgaactccttattgccaaattcagacttaaattgaagaaagtagggaaaaccactagaccattcaggtatgacctaaatcaaatcccttatgattatacagtggaagtgagaaatagatttaagggcctagatctgatagatagagtgcctgatgaactatggaatgaggttcgtgacattgtacaggagacagggatcaagaccatccccatggaaaagaaatgcaaaaaagcaaaatggctgtctg"
+
+	pp := &blast.PutParameters{
+		Program:     program,
+		Database:    "nr",
+		EntrezQuery: "bos taurus[organism]",
+		WordSize:    int(wordSize),
+		Expect:      &expectThreshold,
+		NuclReward:  int(matchScore),
+		NuclPenalty: int(mismatchScore)}
+
+	var gp *blast.GetParameters
+	gp = nil
+
+	r, err := blast.Put(query, pp, tool, email)
+	if err != nil {
+		panic(err)
+	}
+
+	var o *blast.Output
+	for k := 0; k < retry; k++ {
+		// Wait for RTOE to elapse and get search status.
+		var s *blast.SearchInfo
+		s, err = r.SearchInfo(tool, email)
+		if err != nil {
+			panic(err)
+		}
+
+		// Output search status.
+		fmt.Println(s)
+
+		switch s.Status {
+		case "WAITING":
+			continue
+		case "FAILED":
+			fmt.Printf("search: %s failed", r)
+			return
+		case "UNKNOWN":
+			fmt.Printf("search: %s expired", r)
+			return
+		case "READY":
+			if !s.HaveHits {
+				fmt.Printf("search: %s no hits", r)
+				return
+			}
+		default:
+			fmt.Printf("An unknown error occurred.")
+			return
+		}
+
+		// We have hits, so get the BLAST output.
+		o, err = r.GetOutput(gp, tool, email)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(o.Program)
+		return
+	}
+
+	fmt.Printf("%s exceeded retries", r)
+	return
 
 }
